0c95bbc19c1324ea606f8e5a23c0d5be041f1e97
Arqam <arqamafaq03@gmail.com>
2025-04-12 20:01:53 +0530

    Created the datepicker Component function that renders the shell of the app and injects it into #app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import './style.css'
-
+import "./style.css";
+import { Datepicker } from "./datepicker";
 
 // Mount to #app
-const app = document.getElementById("app");
+const appRoot = document.getElementById("app");
+appRoot.className="w-full h-screen flex justify-center items-center"
+
+const DatepickerContainer=document.createElement("div");
+DatepickerContainer.className="w-[70%] h-[65%]";
+DatepickerContainer.appendChild(Datepicker())
+
+appRoot.appendChild(DatepickerContainer);
+
