eba0700714edcf016563db97ec9463c74efa1f8d
Arqam <arqamafaq03@gmail.com>
2025-04-13 11:19:19 +0530

    [200~Add basic component structure: input, display, calendar, and actions

---
+++ src/app.js
@@ -0,0 +1,72 @@
+
+/*main component - bootstraps everything*/
+export default function app() {
+  const app = document.createElement("div");
+  app.id = "picker-app";
+
+  
+  return app;
+}
+
+/*components */
+
+function DatePickerToggle() {
+  const button = document.createElement("button");
+  button.textContent = "Select Date";
+  button.className = "toggle-button";
+  return button;
+}
+
+function calender() {
+  const calender = document.createElement("div");
+  calender.className = "calender";
+  calender.textContent = "Calender will be here";
+
+  return calender;
+}
+
+function calenderHeader() {
+  const header = document.createElement("div");
+  header.textContent = "Header - Month & year + buttons";
+  return header;
+}
+
+function calenderBody() {
+  const body = document.createElement("div");
+  body.textContent = "Dates grid";
+  return body;
+}
+
+function DateSelectionDisplay() {
+  const wrapper = document.createElement("div");
+  wrapper.className = "date-selection-display";
+
+  const start = document.createElement("div");
+  start.className = "start-date";
+  start.textContent = "Start Date";
+
+  const end = document.createElement("div");
+  end.className = "end-date";
+  end.textContent = "End Date";
+
+  wrapper.appendChild(start);
+  wrapper.appendChild(end);
+
+  return wrapper;
+}
+
+function calenderActions() {
+  const actions = document.createElement("div");
+  actions.className = "calender-actions";
+
+  const cancelBtn = document.createElement("button");
+  cancelBtn.textContent = "Cancel";
+
+  const applyBtn = document.createElement("button");
+  applyBtn.textContent = "Apply";
+
+  actions.appendChild(cancelBtn);
+  actions.appendChild(applyBtn);
+
+  return actions;
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import "./style.css";
 import { Datepicker } from "./datepicker";
+import app from "./app";
 
 // Mount to #app
-const appRoot = document.getElementById("app");
-appRoot.className="w-full h-screen flex justify-center items-center"
+const appRoot = document.getElementById("root");
+appRoot.className = "w-full h-screen flex justify-center items-center";
 
-const DatepickerContainer=document.createElement("div");
-DatepickerContainer.className="w-[70%] h-[65%]";
-DatepickerContainer.appendChild(Datepicker())
+const DatepickerContainer = document.createElement("div");
+DatepickerContainer.className = "w-[70%] h-[65%]";
+DatepickerContainer.appendChild(Datepicker());
 
 appRoot.appendChild(DatepickerContainer);
 
+const mainApp = app();
+DatepickerContainer.appendChild(mainApp);
