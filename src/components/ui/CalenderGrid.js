b250a17e084683dde56146d545a531f9b933ee37
Arqam <arqamafaq03@gmail.com>
2025-04-17 11:43:25 +0530

    refactored calendergrid with weekDays as headerRow

--- src/components/ui/CalenderGrid.js
+++ src/components/ui/CalenderGrid.js
@@ -1,21 +1,45 @@
-import { format, getUnixTime } from "date-fns";
+import { format, getUnixTime, isSameMonth, isSameDay } from "date-fns";
 import generateCalenderGrid from "../../utils/generateCalenderDates";
 
-export default function CalenderGrid(selectedDate){
+export default function CalenderGrid(selectedDate) {
+  const dates = generateCalenderGrid(selectedDate);
 
-    const dates = generateCalenderGrid(selectedDate);
+  const container = document.createElement("div");
 
-    const grid = document.createElement("div");
-    grid.className = "grid grid-cols-7 gap-1";
+  // Weekday headers (starting from Monday)
+  const weekdays = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+  const headerRow = document.createElement("div");
+  headerRow.className = "grid grid-cols-7 gap-1 mb-2";
 
-    dates.forEach((date)=>{
-        const day = document.createElement("button");
-        day.textContent=format(date,"d");
-        day.id=getUnixTime(date);
+  weekdays.forEach((day) => {
+    const label = document.createElement("div");
+    label.textContent = day;
+    label.className =
+      "font-medium text-lg grid place-items-center border border-zinc-400 aspect-square";
+    headerRow.appendChild(label);
+  });
 
-        grid.appendChild(day);
-    });
+  container.appendChild(headerRow);
 
-    return grid;
+  //dates grid
+  const grid = document.createElement("div");
+  grid.className = "grid grid-cols-7 gap-1";
 
+  dates.forEach((date) => {
+    const day = document.createElement("button");
+    day.textContent = format(date, "dd");
+    day.className = `place-items-center aspect-square border border-zinc-200 hover:bg-blue-300 cursor-pointer ${
+      isSameMonth(date, selectedDate) ? "text-black" : "text-zinc-400"
+    } ${
+      isSameDay(date, selectedDate) && isSameMonth(date, selectedDate)
+        ? "bg-blue-500 text-white"
+        : ""
+    }`;
+    day.id = getUnixTime(date);
+
+    grid.appendChild(day);
+  });
+
+  container.appendChild(grid);
+  return container;
 }
